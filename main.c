9fe39d4ad5922d0d27e253c463ea312861765fa9
Alan Zlykh <cigikpugik@gmail.com>
2022-09-28 12:36:20 +0300

    Add nickname

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include "askname.h"
 int main(int argc, char **argv)
 {
- char first[255], last[255];
+ char first[255], last[255], nick[255];
  askname(first, last);
- printf("Hello, %s %s!\n", first, last);
+ printf("Hello, %s %s %s!\n", first, last, nick);
  return 0;
 }
